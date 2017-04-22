109cc98391a97e87e5f50936b1c0e10685b6bb46
Madliss <madlessc@gmail.com>
2017-04-22 18:03:56 +0100

    Add files via upload

---
+++ js/wb.js
@@ -0,0 +1,105 @@
+	var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Your location is here.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+      
+      
+      
+      var $ = function(id){
+    		return document.getElementById(id);
+    	}
+
+    	var getHTTPObject = function(){
+    	  var xhr = false;
+    	  if (window.XMLHttpRequest) {
+    	    xhr = new XMLHttpRequest();
+    	  } else if (window.ActiveXObject) {
+    	    try {
+    	      xhr = new ActiveXObject("Msxml2.XMLHTTP");
+    	    } catch(e) {
+    	      try {
+    	        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    	      } catch(e) {
+    	        xhr = false;
+    	      }
+    	    }
+    	  }
+    	  return xhr;
+    	}
+
+    	var getWeatherURL = function(url){
+    		var request = getHTTPObject();
+    		if(request){
+    			request.onreadystatechange = function(){
+    				parseResponse(request);
+    			};
+    			request.open("GET",url,true);
+    			request.send(null);
+    		}
+    	}
+
+    	var parseResponse = function(request){
+    	  if (request.readyState == 4) 
+    	    if (request.status == 200 || request.status == 304) {
+    	      
+    	      console.log(request.responseXML);
+
+    	      //Parse xml file
+    	      var xmlDoc = request.responseXML;
+
+
+    	        var table2 = "<tr></tr>";
+
+    	        var x = xmlDoc.getElementsByTagName("temperature")[0];
+    	        var wea = xmlDoc.getElementsByTagName("symbol")[0].getAttribute("name");
+    	         table2 += "<tr><td>"+"Today:"+x.getAttribute("day")+"&#8451"+"</td></tr><tr><td>"+"Weather description: "+wea+"</td></tr>";
+    	        document.getElementById("demo").innerHTML = table2;
+
+
+    	    }
+    	}
+
+
+    	window.onload = function(){
+
+    		$("searchButton").onclick = function(){
+    			var city = $("city").value;
+    			console.log(city);
+
+    			var url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&mode=xml&units=metric&cnt=7&appid=04cc5da4078d26b707f06cb67a00ec3a";
+    	    getWeatherURL(url);
+    	    document.getElementById("text").innerHTML=$("city").value;
+    		}
+
+    		$("city").focus();
+    	}
